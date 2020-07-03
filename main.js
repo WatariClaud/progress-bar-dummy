bdd2e35e64e84378bf7b8cf93f5dc454144a3349
Claud Watari <claudwatari95@gmail.com>
2020-07-04 00:15:16 +0300

    Vanilla js progress bar - reusable

---
+++ main.js
@@ -0,0 +1,50 @@
+const blogImage = document.querySelectorAll('.d-none')[1];
+const name = document.querySelector('.inputfield');
+const county = document.querySelector('#input-county');
+const submit = document.querySelector('#addBlog');
+const _alert = document.querySelector('#alert-container');
+
+
+const showProgress = () => {
+    var width = 0;
+    var id = setInterval(frame, 100);
+    function frame() {
+      if (width >= 100) {
+        clearInterval(id);
+      } else {
+        width++;
+        _alert.classList.remove('d-none');
+        _alert.style.background = "green";
+        _alert.textContent = `Creating your blog: ${width * 1 }%`;
+      }
+    }
+  }
+
+const checkInput = (event) => {
+    event.preventDefault();
+    if(blogImage.files.length === 0) {
+        _alert.classList.remove('d-none'); _alert.textContent = 'No file selected';
+        return false;
+    }
+    if(!name.value) {
+        _alert.classList.remove('d-none'); _alert.textContent = 'Enter a name';
+        return false;
+    }
+    if(county.options[county.selectedIndex].value === 'null') {
+        _alert.classList.remove('d-none'); _alert.textContent = 'Select a county';
+        return false;
+    }
+    showProgress();
+
+    /* do here any backend processes, like uploading the data to firebase in the backend.
+    Firebase has a way to fetch the progress percentage:
+
+    More details below:
+    https://firebase.google.com/docs/storage/web/upload-files
+
+    You can resuse my html and add styles as you wish. I used colors for the alert section based on result
+    Reddish for errors and green for success. Of course you can change this also.
+    */
+};
+
+submit.addEventListener('click', checkInput);
